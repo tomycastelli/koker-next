e2dc67c09fadbdb9f7f2482ac337f654de205354
Cobancho <tomycastelli@gmail.com>
2025-03-26 16:48:43 -0300

    delete team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { AboutSection } from '@/components/AboutSection'
 import { ContactSection } from '@/components/ContactSection'
 import { HeroSection } from '@/components/HeroSection'
-import { TeamSection } from '@/components/TeamSection'
 
 export default function Home() {
 	return (
 		<main>
 			<HeroSection />
 			<AboutSection />
-			<TeamSection />
 			<ContactSection />
 		</main>
 	)
